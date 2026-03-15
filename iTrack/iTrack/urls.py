2c42bff40bcd24cefb788e630012a7e6229fa140
Syd7 <sydpaguio@gmail.com>
2026-03-15 23:03:40 +0800

    adjusted base url

--- iTrack/iTrack/urls.py
+++ iTrack/iTrack/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
     path('users/', include('user_management.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/user/login/', permanent=False)),
+
 ]
 
